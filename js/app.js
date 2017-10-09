80f0b4efefb1435f6a8c0b4e56bf6bfc2aa2838a
jsadler98 <joshy.sadler@btinternet.com>
2017-10-09 20:34:37 +0100

    added styling to my game

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-const $unicorn = $('.unicorn');
+$(() => {
+  const $unicorn = $('.unicorn');
 
-$(window).on('keydown', function(e) {
-  if(e.keyCode == 37) { // when the user clicks left arrow key the unicorn moves left
-    $('.unicorn').animate({'left': '1px'});
+  function moveUnicorn(){
+    $(window).on('keydown', function(e) {
+      const unicornPosition = $unicorn.position();
+
+      if(e.keyCode == 37) {
+        $unicorn.animate({'left': '-=70px'});
+      }else if (e.keyCode == 39) {
+        $unicorn.animate({'left': '+=70px'});
+      }
+    });
   }
-  else if(e.keyCode == 39) { // when the user clicks right arrow key the unicorn moves right
-    $('.unicorn').animate({'left': '246px'});
-  }
+  moveUnicorn();
+
+  $(window).on('keyup', function() {
+    $('.unicorn').stop();
+  });
 });
+
 
 
 // $(() => {
