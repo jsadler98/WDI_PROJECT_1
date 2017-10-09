f8e6a106c96c432b93ec7340265c0aa6d53f7da7
jsadler98 <joshy.sadler@btinternet.com>
2017-10-09 11:25:50 +0100

    added the function to move my div

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('joel');
+const $unicorn = $('.unicorn');
+
+$(window).on('keydown', function(e) {
+  if(e.keyCode == 37) { // when the user clicks left arrow key the unicorn moves left
+    $('.unicorn').animate({'margin-left': "1px"});
+  }
+  else if(e.keyCode == 39) { // when the user clicks right arrow key the unicorn moves right
+    $('.unicorn').animate({'margin-left': "246px"});
+  }
+});
