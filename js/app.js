1fa3f16b0b6142abfdf6ca00e25f87df05dbba0a
jsadler98 <joshy.sadler@btinternet.com>
2017-10-09 18:17:39 +0100

    box is moving now but not within certain space

--- js/app.js
+++ js/app.js
@@ -2,9 +2,39 @@ const $unicorn = $('.unicorn');
 
 $(window).on('keydown', function(e) {
   if(e.keyCode == 37) { // when the user clicks left arrow key the unicorn moves left
-    $('.unicorn').animate({'margin-left': "1px"});
+    $('.unicorn').animate({'left': '1px'});
   }
   else if(e.keyCode == 39) { // when the user clicks right arrow key the unicorn moves right
-    $('.unicorn').animate({'margin-left': "246px"});
+    $('.unicorn').animate({'left': '246px'});
   }
 });
+
+
+// $(() => {
+//   const $unicorn = $('.unicorn');
+//   const unicornPosition = $unicorn.position().left
+//
+//   function moveUnicorn(){
+//     $(window).on('keydown', function(e) {
+//       // if ($unicornPosition > 0 && $unicornPosition < 250) {
+//
+//       if (unicornPosition > 0  && unicornPosition <500 ) {
+//         if(e.keyCode == 37) {
+//         $unicorn.animate({'margin-left': '-=90px'});
+//         }else if (e.keyCode == 39) { // when the user clicks right arrow key the unicorn moves right
+//           $unicorn.animate({'margin-left': '+=90px'});
+//           console.log(unicornPosition);
+//         }
+//       }
+//
+//     });
+//   }
+//
+//   $(window).on('keyup', function() {
+//     $('.unicorn').stop();
+//   });
+//   };
+// });
+// }
+// moveUnicorn();
+// })
